39e875db0a71e7e8742cb3c6cc0be63f44902708
necoian <0108.facultad@gmail.com>
2025-11-03 21:20:45 -0300

    Se crea formulario en Contacto
    Se crea formulario con sus respectivas verificaciones, falta crear el envio de la misma

--- JS/script.js
+++ JS/script.js
@@ -1,105 +1,193 @@
 // CARROUSEL ----------------------------------------------------------
-
-//constructor de items para el carrousel
-function item(url, nombre, descripcion) {
-    this.url = url;
-    this.nombre = nombre;
-    this.descripcion = descripcion;
-}
-
-//Array del carrousel
-let imagenes = new Array();
-
-//Array del punto Seleccionado
-let puntoPosicion = new Array();
-
-//Se rellena con los items que deseemos
-imagenes.push(new item(
-    "../Imagenes/Proyecto_Ian_1/Portafolio_1.png",
-    "Portafolio de Ian Programacion",
-    " ... "));
-
-imagenes.push(new item(
-    "../Imagenes/Proyecto_Nerina_1/Portafolio_1.png",
-    "Portafolio de Nerina Programacion",
-    " ... "));
-
-imagenes.push(new item(
-    "../Imagenes/Proyecto_Nerina_1/Fidelidad Alta.png",
-    "Boceto de los trabajos realizados",
-    " ... "));
-
-imagenes.push(new item(
-    "../Imagenes/Proyecto_Ian_1/cod_1.png",
-    "Código de trabajo",
-    " ... "));
-
-let izquierda = document.getElementById('izquierda');
-let derecha = document.getElementById('derecha');
-let imagen = document.getElementById('imagenes');
-let puntos = document.getElementById('punto');
-//let texto = document.getElementById();
-let posicion = 0;
-
-
-posicionCarrousel();
-
-function cambiarImagen(posicion) {
-
-    imagen.innerHTML = `<img src="${imagenes[posicion].url}" alt="item" class="item" loading="lazy">`;
-
-}
-
-
-
-izquierda.addEventListener('click', function() {
-
-    posicion--;
-
-    if(posicion == -1) {
-        posicion = imagenes.length - 1;
+if(document.title === "Presentación de Portafolio") {
+    //constructor de items para el carrousel
+    function item(url, nombre, descripcion) {
+        this.url = url;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
-    cambiarImagen(posicion);
+    //Array del carrousel
+    let imagenes = new Array();
+
+    //Array del punto Seleccionado
+    let puntoPosicion = new Array();
+
+    //Se rellena con los items que deseemos
+    imagenes.push(new item(
+        "../Imagenes/Proyecto_Ian_1/Portafolio_1.png",
+        "Portafolio de Ian Programacion",
+        " ... "));
+
+    imagenes.push(new item(
+        "../Imagenes/Proyecto_Nerina_1/Portafolio_1.png",
+        "Portafolio de Nerina Programacion",
+        " ... "));
+
+    imagenes.push(new item(
+        "../Imagenes/Proyecto_Nerina_1/Fidelidad Alta.png",
+        "Boceto de los trabajos realizados",
+        " ... "));
+
+    imagenes.push(new item(
+        "../Imagenes/Proyecto_Ian_1/cod_1.png",
+        "Código de trabajo",
+        " ... "));
+
+    let izquierda = document.getElementById('izquierda');
+    let derecha = document.getElementById('derecha');
+    let imagen = document.getElementById('imagenes');
+    let puntos = document.getElementById('punto');
+    //let texto = document.getElementById();
+    let posicion = 0;
+
 
     posicionCarrousel();
 
-});
+    function cambiarImagen(posicion) {
 
-derecha.addEventListener('click', function() {
+        imagen.innerHTML = `<img src="${imagenes[posicion].url}" alt="item" class="item" loading="lazy">`;
 
-    posicion++;
-
-    if(posicion == imagenes.length) {
-        posicion = 0;
     }
 
-    cambiarImagen(posicion);
 
-    posicionCarrousel();
 
-});
+    izquierda.addEventListener('click', function() {
 
-function posicionCarrousel() {
+        posicion--;
 
-    puntos.innerHTML = "";
+        if(posicion == -1) {
+            posicion = imagenes.length - 1;
+        }
 
-    for (let i = 0 ; i  < imagenes.length ; i++) {
-        
-        if (i == posicion) {
-            puntos.innerHTML += `<p class="puntoSeleccionado">.</p>`
+        cambiarImagen(posicion);
+
+        posicionCarrousel();
+
+    });
+
+    derecha.addEventListener('click', function() {
+
+        posicion++;
+
+        if(posicion == imagenes.length) {
+            posicion = 0;
+        }
+
+        cambiarImagen(posicion);
+
+        posicionCarrousel();
+
+    });
+
+    function posicionCarrousel() {
+
+        puntos.innerHTML = "";
+
+        for (let i = 0 ; i  < imagenes.length ; i++) {
+            
+            if (i == posicion) {
+                puntos.innerHTML += `<p class="puntoSeleccionado">.</p>`
+                
+            }
+            else {
+
+                puntos.innerHTML += `<p>.</p>`         
+            }
             
         }
-        else {
 
-            puntos.innerHTML += `<p>.</p>`         
-        }
-        
     }
-
 }
+
 
 
 
 
 //FIN CARROUSEL -----------------------------------------------------------
+
+
+//---------------------------- FORMULARIO ----------------------------------------
+
+if (document.title === "Contacto") {
+    
+}
+
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit" , function(event) {
+
+    const nombreCompleto = document.getElementById("nombreApellido");
+    const email = document.getElementById("mail");
+    const telefono = document.getElementById("telefono");
+    const mensaje = document.getElementById("mensaje");
+
+    const nombreCompletoError = document.getElementById("errorNombre");
+    const emailError = document.getElementById("errorEmail");
+    const telefonoError = document.getElementById("errorTelefono");
+    const mensajeError = document.getElementById("errorMensaje");
+
+    const nombreER = /^.{10,50}$/; 
+    const emailER = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+    const telefonoER = /^[0-9]{6,13}$/;
+    const mensajeER = /^[\s\S]{10,512}$/;
+
+    let valido = true;
+
+    if (!nombreER.test(nombreCompleto.value)) {
+        valido = false;
+        nombreCompletoError.textContent = "Se permite entre 10 a 50 caracteres";
+    } else {
+        nombreCompletoError.textContent = "";
+        nombreCompleto.setCustomValidity("");
+        
+    }
+
+    if (!emailER.test(email.value)) {
+        valido = false;
+        emailError.textContent = "No es correcto el formato de correo";
+    } else {
+        emailError.textContent = "";
+        email.setCustomValidity("");
+    }
+
+    if (!telefonoER.test(telefono.value)) {
+        valido = false;
+        telefonoError.textContent = "formato solo numerico";
+    } else {
+        telefonoError.textContent = "";
+        telefono.setCustomValidity("");
+    }
+
+    if (!mensajeER.test(mensaje.value)) {
+        valido = false;
+        mensajeError.textContent = "Se puede colocar entre 10 a 512 caracteres";
+    } else {
+        mensajeError.textContent = "";
+        mensaje.setCustomValidity("");
+    }
+
+    if (!valido) {
+        formulario.reportValidity();
+        event.preventDefault();
+    } else {
+        const informacion = {
+            nombreCompleto: nombreCompleto.value,
+            email: email.value,
+            telefono: telefono.value,
+            mensaje: mensaje.value
+        };
+    }   
+}, true);
+
+
+
+
+
+
+
+
+
+
+//---------------------------- FIN FORMULARIO ----------------------------------------
